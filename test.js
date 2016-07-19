725cb44de640085d12fa18444d650a024b287bfc
jekrb <Jacob.JW.Burden@gmail.com>
2016-07-19 18:35:10 -0400

    add tape test and update package.json

--- test.js
+++ test.js
@@ -1,17 +1,32 @@
+const test = require('tape');
 
 // map - map a value from one range to another
 const map = require('./map');
-console.log('map()', map(5, {
-	min: 0,
-	max: 10
-}, {
-	min: 50,
-	max: 100
-}));
+test('map()', function (t) {
+  t.plan(1)
+  
+  const expected = 75;
+  const res = map(5, {
+    min: 0,
+    max: 10
+  }, {
+    min: 50,
+    max: 100
+  });
+  
+  t.same(res, expected);
+})
 
 // hexMix - return hex value at % point between two colors
 const hexMix = require('./hexMix');
-console.log('hexMix()', hexMix(.5, {
-	from: '#f39631',
-	to: '#ed4c7c'
-}));
+test('hexMix()', function (t) {
+  t.plan(1) 
+
+  const expected = '#F07157';
+  const res = hexMix(.5, {
+    from: '#f39631',
+    to: '#ed4c7c'
+  });
+  
+  t.same(res, expected);
+})
