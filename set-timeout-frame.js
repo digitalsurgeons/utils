a4480f66b91db20273bb824ea7ef72d134be4ed6
Adam Chambers <adammchambers@gmail.com>
2016-07-31 18:53:17 -0400

    First iteration of set-timeout-frame module

--- set-timeout-frame.js
+++ set-timeout-frame.js
@@ -4,5 +4,11 @@
 * func() //
 */
 module.exports = function(func, delay = 0) {
+	if(typeof func !== 'function') {
+		return false;
+	}
 
+	return setTimeout(function bindScope(func) {
+		requestAnimationFrame(func);
+	}.bind(this, func), delay);
 };
