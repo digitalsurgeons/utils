fe56f3c270b3d9a3607f4d01a5dfb07a14884c1f
Adam Chambers <adammchambers@gmail.com>
2016-07-31 20:03:53 -0400

    Add display function to fade elements in / out, setting display followed by opacity after short delay

--- display-fade.js
+++ display-fade.js
@@ -1,25 +1,35 @@
 /*
-* display-fade -
+* display-fade (ensure transition set on element in CSS)
+*    in - set display block then opacity 1 after short delay
+*    out - set opacity 0 then display none after short delay
 *
-* func() //
+* func(document.querySelector('.element'), 1000, () => { console.log('done!'); });
 */
 
-const setTimeoutFrame = require('set-timeout-frame');
+const setTimeoutFrame = require('./set-timeout-frame');
 
 module.exports = {
-	in: (element, delay = 100) => {
+	in: (element, delay = 100, func) => {
 		element.style.display = 'block';
 
-		setTimeoutFrame(function bindScope(element) {
+		setTimeoutFrame(function bindScope(element, func) {
 			element.style.opacity = 1;
-		}.bind(this, element), delay);
+
+			if(typeof func === 'function') {
+				func();
+			}
+		}.bind(this, element, func), delay);
 	},
 
-	out: (element, delay = 300) => {
+	out: (element, delay = 300, func) => {
 		element.style.opacity = 0;
 
-		setTimeoutFrame(function bindScope(element) {
+		setTimeoutFrame(function bindScope(element, func) {
 			element.style.display = 'none';
-		}.bind(this, element), delay);
+
+			if(typeof func === 'function') {
+				func();
+			}
+		}.bind(this, element, func), delay);
 	}
 };
